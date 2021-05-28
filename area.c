418d4371446a48de7c3d9d37cccbe0d61d07f12a
S Pranav <65620582+sreenathpranav@users.noreply.github.com>
2021-05-28 23:57:59 +0530

    1st question
    Find area of a rectangle

---
+++ area.c
@@ -0,0 +1,21 @@
+//Q1. Write a c program to calculate the area of a rectangle:
+//a) using hardcoded inputs & 
+//b) using inputs supplied by the user
+
+#include<stdio.h>
+void main()
+{
+
+/*int length=3, breadth=8;
+int area= length* breadth;
+printf("The area of this rectangle is %d",area);*/
+
+int length,breadth;
+printf("What is the length of the rectangle\n");
+scanf("%d", &length);
+
+printf("What is the breadth of the recatangle\n");
+scanf("%d",&breadth);
+
+printf("The area of this rectangle is %d", length*breadth);
+}
