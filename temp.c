8e18d5961433fae5307abcc122441e6a15e9293a
S Pranav <65620582+sreenathpranav@users.noreply.github.com>
2021-05-29 23:54:58 +0530

    2nd question

--- temp.c
+++ temp.c
@@ -1,7 +1,8 @@
 //Q3. Write a program to convert Celsius (Centigrade degrees temperature to Fahrenheit)
 
-#include <stdio.h>
-void main()
-{
-
+#include<stdio.h>
+void main(){
+    float celsius = 37h,far;
+    far =(celsius *9/5)+32;   //equation
+  printf("The value of this celsius temperature in Fahrenheit is %f",far);
 }
