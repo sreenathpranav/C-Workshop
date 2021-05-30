6371c8b02808b56ab60174533355b83d01672476
S Pranav <65620582+sreenathpranav@users.noreply.github.com>
2021-05-30 23:23:22 +0530

    Question 3

---
+++ volume.c
@@ -0,0 +1,12 @@
+/*Q3. Calculate the area of a circle and modify the same program to calculate the volume of a cylinder given its radius and height.*/
+
+#include<stdio.h>
+void main()
+{
+int radius=3;
+float pi=3.14;
+printf("The area of this circle is %f\n", pi*radius*radius);
+
+int height=5;
+printf("The voloume of this cylinder is %f\n", pi*radius*radius*height);
+}
