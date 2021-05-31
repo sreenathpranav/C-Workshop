2aab15fe12a9d5f99b39cfa495a7e29f43751ce0
S Pranav <65620582+sreenathpranav@users.noreply.github.com>
2021-05-31 10:05:31 +0530

    Question 5

--- large.c
+++ large.c
@@ -4,10 +4,10 @@
 void main()
 {
 int a,b;
-printf("Enter the two numbers:");
+printf("Enter the two numbers: ");
 scanf("%d%d",&a,&b);
 
-if(a<b){
+if(a>b){
 printf("The largest number is %d\n",a);
 }
 else{
