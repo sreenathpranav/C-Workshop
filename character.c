3ca452249e3ba910c3465bb6a533cd33c8fb9193
S Pranav <65620582+sreenathpranav@users.noreply.github.com>
2021-05-31 09:36:01 +0530

    Question 8

---
+++ character.c
@@ -0,0 +1,16 @@
+//Qn.8 Program to read an alphabet and check whether the given alphabet is 'a' or 'e'
+
+#include<stdio.h>
+void main()
+{
+    char character;
+    printf("Enter the character in small letters:");
+    scanf("%c",&character);
+    if(character=='a' || character =='e' )   //OR operator
+    {
+        printf("TRUE\n");
+    }
+    else{
+        printf("FALSE\n");
+    }
+}
